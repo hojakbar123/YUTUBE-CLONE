b716e9bebd221c2195ce16c05742184962d92d71
hojakbar <fgfcfdhgv@gmail.com>
2025-01-27 15:06:23 +0500

    first commit

--- src/pages/SearchFeed.jsx
+++ src/pages/SearchFeed.jsx
@@ -1,44 +1,51 @@
 import React, { useEffect, useState } from 'react'
+import Videos from '../Components/Videos' 
 import { useParams } from 'react-router-dom';
-import Videos from '../Components/Videos';
-import { $api } from '../utils';
-import Loader from '../Components/loader';
-export default function SearchFeed() {
+import Loader from '../Components/Loader'; 
+import {$api} from '../utils/index';
+function SearchFeed() {
   const [videos, setVideos] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const { searchTerm } = useParams();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await $api.get(
-         ` /search?part=snippet&q=${searchTerm}&maxResults=50`
+          `/search?part=snippet&q=${searchTerm}&maxResults=50`
         );
         if (response.status === 200) {
-          setVideos(response.data.items);
+          setVideos(response.data.items);  
           setLoading(false);
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     };
+
     fetchData();
   }, [searchTerm]);
+
   if (loading) {
     return (
-      <div className='flex bg-black justify-center items-center h-screen'>
-<Loader/>
+      <div className="flex bg-black justify-center items-center h-screen">
+        <Loader />
       </div>
-    )
+    );
   }
+
   return (
-    <div className='min-h-[92vh] bg-black'>
-      <div className='max-w-7xl mx-auto'>
-        <h1 className='text-white text-5xl font-bold'>Search results <span className='text-cherry'> 
-          {searchTerm}</span> videos</h1>
+    <div className="min-h-[92vh] bg-black">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-white text-5xl font-bold">
+          Search result for <span className="text-cherry">{searchTerm}</span> videos
+        </h1>
       </div>
-      <div className='p-4 flex mx-auto px-4 lg:px-[100px]'>
+      <div className="p-4 flex mx-auto w-full px-4 lg:px-[100px]">
         <Videos videos={videos} />
       </div>
     </div>
-  )
+  );
 }
+
+export default SearchFeed;
